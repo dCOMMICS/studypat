623bca5e1cf9905b5cc8596761ccb1fdce4397aa
dCOMMICS <sanwurld@gmail.com>
2023-09-27 20:41:00 +0300

    deleted some lines with were not working

--- studypat/settings.py
+++ studypat/settings.py
@@ -7,7 +7,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
+
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-29^v-z%(w97_yk#1g330+476^3p0r^rtv%8zf(6fa+lrbp&__n'
